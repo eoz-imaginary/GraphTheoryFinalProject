fecfecc0d5ae12ae22d769a36954b8031dd5929b
Tasaday Green <tgreen@smith.edu>
2018-04-30 01:06:35 -0400

    fixed leastPop

--- greedyColors.m
+++ greedyColors.m
@@ -6,13 +6,13 @@ colorList = zeros(1, states);
 v(1, 1) = 1; %assigning color 1 to a random starting node
 for i=1:states % go through all states
     for j=1:states % goes through column of neighbors for each state in A
-         if (A(i, j) == 1)
-             colorList(1, j) = v(1, j);             
+         if (A(i, j) == 1) %checks if nodes are adjacent
+             colorList(1, j) = v(1, j); %adds neighbor's color to colorList            
          end
     end
     color = min(setdiff(1:states, colorList)); %intersection of sets
-    v(1, i) = color;
-    colorList=zeros(1,states);
+    v(1, i) = color; %assigns lowest color not used by neighbors
+    colorList=zeros(1,states); %resets colorList
 end
 
 
--- leastPop.m
+++ leastPop.m
@@ -2,21 +2,22 @@ function[v] = leastPop(n, states)
 Map=expansion(n, states);
 A=makeAdj(n, states)
 v=zeros(1,states); %empty vector that will be filled in with colors
+colorList = zeros(1,states);
 degSum = sum(A); %store degrees of each node
-colorList = zeros(1, states);
-index = find(degSum==min(degSum)); %initial index of lowest degree node
-for i=1:states %loop through rows   
-    for j = 1:states %loop column-times      
-        if (A(i, index(1)) == 1)             
-            colorList(1, index(1)) = v(1, index(1));           
-        end       
-    end    
-    color = min(setdiff(1:states, colorList)); %smallest unused color
-    index = find(degSum==min(degSum)); %index of lowest degree node 
-    %if (min(degSum) ~= Inf) %if we haven't 'used up' our degSum, continue as usual
-        v(1, index(1)) = color;
-        degSum(index(1)) = Inf;
-   % else %else color the last color and end
-   %     v(1, index(1)) = color; 
-   % end    
+index = find(degSum==min(degSum)); %initial index of lowest degree nodes
+v(1,index(1)) = 1; %assigns node of lowest degree color 1
+degSum(index(1)) = inf; %sets that node to degree infinity
+for n = 1:states %cycles through each node
+    index = find(degSum==min(degSum)); % index of lowest degree nodes
+    for i=1:states % goes through column of neighbors for each state in A
+         if (A(i, index(1)) == 1) %if node is ajdacent to A(i,index(1))...
+             colorList(1, i) = v(1, i); %add neighbor's color to colorList             
+         end
+         
+    end
+    color = min(setdiff(1:states, colorList)); %intersection of sets
+    v(1, index(1)) = color; %assigns lowest color not used by neighbors
+    colorList=zeros(1,states); %resets colorList
+    degSum(index(1))=inf; %sets degree of node to infinity
+    
 end
